f9f795f5e3b4e0c8ac3a6f1dbcb41a22373d00f3
grumlebob <grumlebob@gmail.com>
2024-01-30 14:20:30 +0100

    Scroll image

---
+++ KinoPrototype/KinoPrototype.Client/wwwroot/scrolling.js
@@ -0,0 +1,10 @@
+﻿function adjustPosterPosition() {
+    var posterImage = document.querySelector('.PosterImage');
+    if (posterImage) {
+        var redboxRect = posterImage.closest('.Redbox').getBoundingClientRect();
+        var offset = window.pageYOffset + redboxRect.top + (redboxRect.height / 2) - (posterImage.offsetHeight / 2);
+        posterImage.style.top = offset + 'px';
+    }
+}
+
+window.addEventListener('scroll', adjustPosterPosition);
