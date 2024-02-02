a79b6f775988672321331476d25312ab000cf538
alberted <alberted115@gmail.com>
2024-02-02 10:56:19 +0100

    new program.cs

--- KinoPrototype/KinoPrototype.Client/Program.cs
+++ KinoPrototype/KinoPrototype.Client/Program.cs
@@ -5,4 +5,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped<HttpClient>(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
+
 await builder.Build().RunAsync();
